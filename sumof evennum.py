f99885cc117534a2f5f7f09e9d9cec63705adc68
rahulram2006 <raj2004rahul1234@gmail.com>
2025-03-11 12:03:02 +0530

    Create sumof evennum.py

---
+++ sumof evennum.py
@@ -0,0 +1,7 @@
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+even_sum = 0
+for num in range(start, end + 1):
+    if num % 2 == 0:
+        even_sum += num
+print(f"The sum of even numbers between {start} and {end} is: {even_sum}")
