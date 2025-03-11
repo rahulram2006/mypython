3703dc63e57ad27b17df1338de7e4fcaeccd294e
rahulram2006 <raj2004rahul1234@gmail.com>
2025-03-11 10:21:53 +0530

    Create if elif.py

---
+++ if elif.py
@@ -0,0 +1,8 @@
+temperature = 25
+
+if temperature > 30:
+    print("It's really hot outside!")
+elif temperature > 20:
+    print("The weather is nice.")
+else:
+    print("It's a bit chilly outside.")
