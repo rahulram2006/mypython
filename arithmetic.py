b80f9cf755bdb6ec1169706b1bb5c0e22fd1bb58
rahulram2006 <raj2004rahul1234@gmail.com>
2025-03-11 09:33:40 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,35 @@
+num1 = float(input("Enter the first number Loading...
+
+num2 = float(input("Enter the second number: "))
+
+operator = input("Enter the operator (+, -, *, /): ")
+
+{x}
+
+if operator == +":
+
+result = num1 + num2
+
+elif operator ==
+
+result = num1 num2
+
+elif operator == "*":
+
+result = num1 * num2
+
+elif operator == "/":
+
+if num2 != 0:
+
+result = num1 / num2
+
+else:
+
+result = "Error! Division by zero."
+
+else:
+
+result = "Invalid operator!"
+
+print(f"The result is: {result}")
