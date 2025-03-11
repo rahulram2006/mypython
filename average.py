a816e336dca151f69dff3b4f78cca5579e3a7d9d
rahulram2006 <raj2004rahul1234@gmail.com>
2025-03-11 09:40:23 +0530

    Create average.py

---
+++ average.py
@@ -0,0 +1,10 @@
+num1 = float(input("enter first number: "))
+
+num2 float (input ("enter second number: "))
+ num3 = float (input ("enter third number : "))
+
+print("rahul.p")
+
+average (num1 + num2 + num3)
+
+print("The average is:", average)
